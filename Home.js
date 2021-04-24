0ba6048dd37a19e8df04c69f5b4a357d554d9603
mdimrannazir <imrannazir1.in@gmail.com>
2021-04-24 16:34:14 +0600

    Learned Prompt

---
+++ Home.js
@@ -0,0 +1,13 @@
+console.log('Hello');
+// alert('Are you ok');
+
+var x = 'smoothie';
+console.log(x);
+
+var num = '43'
+console.log('num');
+
+var name = prompt('What is your name?');
+document.getElementById('someText').innerHTML = name ;
+
+
