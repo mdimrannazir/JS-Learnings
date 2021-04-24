e9e747bdec8bd5fc6521c6cd6a0ff223d5d32684
mdimrannazir <imrannazir1.in@gmail.com>
2021-04-24 16:52:25 +0600

    Numbers in JS

--- Home.js
+++ Home.js
@@ -7,7 +7,23 @@ console.log(x);
 var num = '43'
 console.log('num');
 
-var name = prompt('What is your name?');
-document.getElementById('someText').innerHTML = name ;
+// var name = prompt('What is your name?');
+// document.getElementById('someText').innerHTML = name ;
 
+// Numbers in Javascript 
+var num1 = 10;
 
+//Increment Num 1 by 1
+num1++;
+console.log(num1);
+
+//Decrement Num 1 by 1
+num1--;
+console.log(num1);
+
+//Devide/ , Multiply* , Reminder %
+console.log(num1 % 6);
+
+// Increment / Decrement by 10
+num1+=0;
+console.log(num1);
