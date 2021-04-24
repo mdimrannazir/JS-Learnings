a9830f1fead760b2758a3241017f9c9c983ee43b
mdimrannazir <imrannazir1.in@gmail.com>
2021-04-24 17:20:02 +0600

    Funtions + Function Arguments in JS

--- Home.js
+++ Home.js
@@ -7,10 +7,13 @@ console.log(x);
 var num = '43'
 console.log('num');
 
-// var name = prompt('What is your name?');
-// document.getElementById('someText').innerHTML = name ;
+// var age = prompt('What is your age?');
+// document.getElementById('someText').innerHTML = 'Your age is ' + age ;
 
-// Numbers in Javascript 
+/* 
+    Numbers in JS   
+*/
+
 var num1 = 10;
 
 //Increment Num 1 by 1
@@ -25,5 +28,47 @@ console.log(num1);
 console.log(num1 % 6);
 
 // Increment / Decrement by 10
-num1+=0;
+num1 += 10;
 console.log(num1);
+
+/* 
+    Functions in JS
+    1. Create A Funtion
+    2. Call the Funtion
+*/
+
+// 1. Create A Funtion
+function fun() {
+    console.log('This is a funtion');
+}
+
+// 2. Call the Funtion
+fun();
+
+/*Lets create a function that take in a name
+and says Hello followed by your name
+
+EX:
+Name: "Imran"
+Return: "Hello Imran"
+*/
+
+function greeting(yourName) {
+    var result = 'Hello' + ' ' + yourName; //String Concatention
+    console.log(result);
+    
+}
+var nameVariable = prompt('What is your name?');
+greeting(nameVariable);
+
+//How do arguments work in Functions?
+//How do we add 2 numbers together in a funtion?
+
+function sumNumbers(num1, num2) {
+    var result = num1 + num2;
+    console.log(result)
+}
+
+sumNumbers(10, 10);
+
+
